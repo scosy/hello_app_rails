2761d3b5af4c30fac2147d8a13b027ea62997922
scosy <amine.bentaleb064@gmail.com>
2022-04-06 07:25:15 +0200

    Changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "application#hello"
-  get "goodbye", to:"application#goodbye"
+  # root "application#hello"
+  root "goodbye", to:"application#goodbye"
   # Defines the root path route ("/")
   # root "articles#index"
 end
